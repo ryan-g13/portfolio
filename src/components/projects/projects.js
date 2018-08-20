0df7ea2e0ec9dd264e7f3fb3696c8246a8b7ef81
Ryan Groesch <ryan.groesch@gmail.com>
2018-08-20 15:43:49 -0700

    Projects page layout and resources

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -7,12 +7,29 @@ export default class Projects extends React.Component {
       <div className='projects' >
         <div className='row' >
           <div className='leftItem' >
-            <img src='imageLinkHere' alt='ImageDescription'/>
-            <p><a href='www.eventus.site'><span>EventUs</span></a></p>
+            <img src='../../assets/eventUs-logo.jpg' alt='EventUs logo'/>
+            <p><a href='www.eventus.site'><span>EventUs</span></a> - This application is designed to be a social  media oriented around Events. The concept is that as a user you can sign-up in order to create, view and attend (running and drinking) events that are near the user.
+            </p>
+            <p>
+              The original customer is for a local Hashing club to alleviate the need to post in multiple social media platforms, provide a streamlined UI for events in the area near the user and persist the organized data in an efficient manner.
+            </p>
+            <p>
+              The application utilizes a MERN Stack (MongoDB, Express.js, React/Redux and Node.js) with OAuthorization to accomplish this.
+            </p>
+          </div>
+          <div className='rightItem' >
+            <img src='imageLinkHere' alt='PubHub Logo'/>
+            <p><a href='https://pub-hub.herokuapp.com'><span>PubHub</span></a> - This is a back-end application that generates a pub crawl, including user authentication, persistence of data via MongoDB, and route optimization via GraphHopper API. It uses the Google Places API to populate stops and stores/retrieves data via Mongoose middleware to the database. The application utilizes an Express server built with Node.js, middleware and a SMS send and retrieve feature via a Twilio API.</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='leftItem' >
+            <img src='imageLinkHere' alt='Play&Rest Logo'/>
+            <p><a href='playandrest.us'><span>Play&Rest</span></a> - This application is designed to help the user locate nearby play areas(for children and dogs alike) and water access points. This uses the Google Maps API to populate a map which then has data points added to the display via database calls and Javascript logic. There is a search feature with auto-filling text and filtering options depending on user preference. Note the data is only available currently for the City of Seattle.</p>
           </div>
           <div className='rightItem' >
             <img src='imageLinkHere' alt='ImageDescription'/>
-            <p>Long Description of project # 2 </p>
+            <p>Short Description of project # 2 </p>
           </div>
         </div>
       </div>
