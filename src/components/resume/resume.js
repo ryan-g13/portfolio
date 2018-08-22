0b2fa4afa4d15399a24e6b49657001df368299bd
Ryan Groesch <ryan.groesch@gmail.com>
2018-08-22 16:33:49 -0700

    Resume pdf viewer is working needs alignment

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf';
 import './resume.scss';
+import resume from '../../assets/Ryan Groesch - August2018.pdf';
 
 export default class Resume extends React.Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div className='resume' >
         <h1>This is a placeholder for my resume pdf clickable/downloadable.</h1>
         <button className='download-button'> Download</button> 
-        {/* <embed src="../../assets/Ryan Groesch - August2018.pdf" type="application/pdf"   height="300px" width="100%"></embed> */}
-        <img src='http://via.placeholder.com/480x640' alt='Resume PDF' /> 
+        <Document 
+          file={resume}
+          onLoadSuccess={this.onDocumentLoad} className="resume"
+          width='720'> 
+          <Page pageNumber={pageNumber} />
+          </Document>
+          <p>Page {pageNumber} of {numPages}</p>
       </div>
     );
   }
