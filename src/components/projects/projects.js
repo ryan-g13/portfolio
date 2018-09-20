6210875a85111aa8abb0d635a90faa84d3444a0d
Ryan Groesch <ryan.groesch@gmail.com>
2018-09-20 14:27:36 -0700

    added project text and pivoted away from svg image

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -58,8 +58,19 @@ export default class Projects extends React.Component {
           </div>
           <div className='rightItem' >
             <img src='http://via.placeholder.com/350x150' alt='ImageDescription'/>
-            <p><a href='https://github.com/TimeBoxed' target="_blank" rel="noopener noreferrer"><span>TimeBoxed</span></a> - Virtual Task Manager</p>
-            <p><a href='https://github.com/TimeBoxed' target="_blank" rel="noopener noreferrer"><span>GitHub Repo</span></a></p>
+            <p>
+              <a href='https://github.com/TimeBoxed' target="_blank" rel="noopener noreferrer"><span>TimeBoxed</span></a> - Virtual Task Manager - Standalone task management application designed to integrate with your calendar to identify downtime and fill it with algorithmically ordered tasks. 
+            </p>
+            <p>
+              Tasks can be organized under a project, have dependent tasks or as singular items. In order to streamline the process default preferences can be used and stored to a user's profile.
+            </p>
+            <p> 
+              Full-stack (MERN), Material UI for styling, and OAuth 2.0 to access your calendar.  
+            </p>
+            <p>
+              <a href='https://github.com/TimeBoxed' target="_blank" rel="noopener noreferrer"><span>GitHub Repo</span>
+              </a>
+            </p>
           </div>
         </div>
       </div>
